2d5234af557ae1b1b9a98b75d58e6352b7d80993
Angius <koumarin@gmail.com>
2018-12-05 03:57:56 +0100

    Done

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Numerics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Threading;
@@ -21,27 +22,97 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string buffer = "";
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        //private void Window_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    Task.Run(() => Marquee("WPF")).ConfigureAwait(false);
-        //}
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Buttons responsivness
+            button_col.Width = new GridLength(Height / (3.0 / 2.0));
 
-        //public void Marquee(string text)
-        //{
-        //    int i = 0;
-        //    while (true)
-        //    {
-        //        this.Dispatcher.Invoke(() => {
-        //            Title = i.ToString();
-        //            i = i > text.Length ? 0 : i + 1;
-        //            Thread.Sleep(500);
-        //        });
-        //    }
-        //}
+            // Textobx responsivness
+            double fontsize = (Height - 20 * 3) / 6;
+            val1_tbox.FontSize   = fontsize;
+            val2_tbox.FontSize   = fontsize;
+            result_tbox.FontSize = fontsize;
+        }
+
+        // Operation buttons
+        private void Sum_btn_Click(object sender, RoutedEventArgs e)
+        {
+            result_tbox.Text = new Operation(val1_tbox.Text, val2_tbox.Text).Sum().ToString();
+        }
+
+        private void Sub_btn_Click(object sender, RoutedEventArgs e)
+        {
+            result_tbox.Text = new Operation(val1_tbox.Text, val2_tbox.Text).Subtract().ToString();
+
+        }
+
+        private void Mult_btn_Click(object sender, RoutedEventArgs e)
+        {
+            result_tbox.Text = new Operation(val1_tbox.Text, val2_tbox.Text).Multiply().ToString();
+
+        }
+
+        private void Div_btn_Click(object sender, RoutedEventArgs e)
+        {
+            result_tbox.Text = new Operation(val1_tbox.Text, val2_tbox.Text).Divide().ToString();
+
+        }
+
+        private void Mod_btn_Click(object sender, RoutedEventArgs e)
+        {
+            result_tbox.Text = new Operation(val1_tbox.Text, val2_tbox.Text).Modulo().ToString();
+
+        }
+
+        private void Clr_btn_Click(object sender, RoutedEventArgs e)
+        {
+            val1_tbox.Text   = "";
+            val2_tbox.Text   = "";
+            result_tbox.Text = "";
+        }
+
+        // Copypaste buttons
+        private void Paste_btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Tag)
+            {
+                case "val1":
+                    val1_tbox.Text = buffer;
+                    break;
+
+                case "val2":
+                    val2_tbox.Text = buffer;
+                    break;
+            }
+        }
+
+        private void Copy_btn_Click(object sender, RoutedEventArgs e)
+        {
+            buffer = result_tbox.Text;
+        }
+    }
+
+    class Operation
+    {
+        private readonly BigInteger A, B;
+
+        public Operation(string a, string b)
+        {
+            BigInteger.TryParse(a, out A);
+            BigInteger.TryParse(b, out B);
+        }
+
+        public BigInteger Sum()      => A + B;
+        public BigInteger Subtract() => A - B;
+        public BigInteger Multiply() => A * B;
+        public BigInteger Divide()   => A / B;
+        public BigInteger Modulo()   => A % B;
     }
 }
